b7350face68e5796f14f1497b573b8a1a6c9ed41
Bibek Shrestha Hybris <bibek.shrestha@hybris.com>
2012-09-10 22:47:53 +0200

    FileDownloader class updated

--- usertracker.py
+++ usertracker.py
@@ -6,7 +6,7 @@ from lxml import etree
 
 def log(logString, debug=False):
 	if (not debug) or Config.DEBUG:
-		print logString
+		print(logString)
 
 class Config:
 	ROOTPATH = "."
@@ -31,40 +31,85 @@ class User:
 		else:
 			log("User XML contains no changeset Element", debug=True)
 
+	def getLastElement(self):
+		lastElement = False
+		if len(self._changesetXmlRoot) > 0:
+			lastElement = self._changesetXmlRoot[0]
+		return lastElement
 
-class UserFile:
+	def getChangeSetIdList(self):
+		changesetIdList = []
+		for changesetElement in self._changesetXmlRoot:
+			changesetIdList.append(changesetElement.get('id'))
+		return changesetIdList
+
+class FileDownloader:
 	_isLoaded = False
 	_dataFolder = "userdata"
-	_data = ''
-	_name = ''
-	_user = ''
-	_userChangeSetUrl = "http://api.openstreetmap.org/api/0.6/changesets?display_name={0}"
+	_fileName = ""
+	_url = ""
+	_data = ""
 
-	def __init__(self, name, update=False):
-		self._name = name
+	def __init__(self, fileName, url, dataFolder = None):
+		if (dataFolder):
+			self._dataFolder = dataFolder
+		self._fileName = fileName
+		self._url = url
 
-	def loadData(self):
-		dataPath = Config.ROOTPATH + "/" + self._dataFolder + "/" + self._name + ".xml"
+	def download(self):
+		dataPath = self._dataFolder + "/" + self._fileName
 		if not os.path.exists(dataPath):
 			log(dataPath + " does not exist. Downloading")
-			r = requests.get(self._userChangeSetUrl.format(self._name))
+			r = requests.get(self._url)
 			if r.status_code == 200:
 				f = open(dataPath, "w")
-				f.write(r.text)
-				self._data = r.text
+				f.write(r.text.encode('utf-8'))
+				self._data = r.text.encode('utf-8')
 				self._isLoaded = True
 		else:
 			log(dataPath + " exists already. Skipping Download")
 			f = open(dataPath, "r")
-			self._data = f.read()
+			self._data = f.read().encode('utf-8')
 			self._isLoaded = True
+
+	def isLoaded(self):
+		return self._isLoaded
+
+
+class UserChangeFile(FileDownloader):
+	_fileName = ""
+	_urlTemplate = "http://api.openstreetmap.org/api/0.6/changeset/{0}/download"
+	_url = ""
+
+	def __init__(self, changesetId):
+		self._fileName = changesetId + ".xml"
+		self._url = self._urlTemplate.format(changesetId)
+
+
+
+class UserFile(FileDownloader):
+	_name = ''
+	_user = ''
+	_urlTemplate = "http://api.openstreetmap.org/api/0.6/changesets?display_name={0}"
+
+	def __init__(self, name, update=False):
+		self._name = name
+		self._url = self._urlTemplate.format(name)
+		self._fileName = name + ".xml"
+
+	def loadData(self):
+		self.download()
 
 		if self._isLoaded:
 			self._user = User()
 			self._user.loadChangesetFromString(self._data)
 			self._user.loadValuesFromChangeset()
-			print self._user._uid
-			print self._user._user
+			print(self._user._uid)
+			print(self._user._user)
+			changesetIdList = self._user.getChangeSetIdList()
+			for changesetId in changesetIdList:
+				changesetFile = UserChangeFile(changesetId)
+				changesetFile.download()
 
 	def updateData(self):
 		if self._isLoaded:
@@ -76,7 +121,7 @@ class UserFile:
 		# TODO
 
 def main():
-	# test = UserFile("bimal(Adyota)")
+	#test = UserFile("bimal(Adyota)")
 	test = UserFile("prabhasp")
 	test.loadData();
 
